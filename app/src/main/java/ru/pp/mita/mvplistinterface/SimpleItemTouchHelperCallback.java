f56720b75b65f7d67ec39e887815fd7da2cf887f
mds99 <zhopa@mita.pp.ru>
2017-04-26 02:49:56 +0300

    upd

--- app/src/main/java/ru/pp/mita/mvplistinterface/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/ru/pp/mita/mvplistinterface/SimpleItemTouchHelperCallback.java
@@ -1,11 +1,30 @@
 package ru.pp.mita.mvplistinterface;
 
 
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
 /**
  * Created by mds on 4/26/2017.
  */
 
-public class SimpleItemTouchHelperCallback implements ItemTouchHelper.Callback {
+public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
+
+
+    @Override
+    public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        return 0;
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+
+    }
 
 
 }
